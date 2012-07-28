dde334f9f8da53897963fafde034ab43209dfa39
Brad Sarsfield <bradsa@microsoft.com>
2012-07-28 00:50:57 -0700

    *updates for windows install / uninstall.cmd to log and take external env vars *fix blank service log file names in hdfs.cmd and mapred.cmd

--- src/packages/win/scripts/install.cmd
+++ src/packages/win/scripts/install.cmd
@@ -15,33 +15,58 @@
 @rem limitations under the License.
 setlocal enabledelayedexpansion
 
-set HADOOP_INSTALL_ROOT=c:\hadoop
+@rem
+@rem  Setup envrionment variables
+@rem
+if not defined HADOOP_INSTALL_ROOT ( 
+  set HADOOP_INSTALL_ROOT=c:\hadoop
+)
+if not defined CORE_INSTALL_PATH ( 
+  set CORE_INSTALL_PATH=%~dp0
+)
+if not defined WINPKG_LOG ( 
+  set WINPKG_LOG=%CORE_INSTALL_PATH%\@final.name@-winpkg.log
+)
+set HADOOP_INSTALL_DIR=%HADOOP_INSTALL_ROOT%\@final.name@
+set HADOOP_INSTALL_BIN=%HADOOP_INSTALL_DIR%\bin
 
-echo Installing Apache Hadoop @version@
 
 @rem ensure running as admin.
 reg query "HKEY_USERS\S-1-5-19\Environment" /v TEMP 2>&1 | findstr /I /C:"REG_EXPAND_SZ" 2>&1 > NUL
 If %ERRORLEVEL% EQU 1 (
-  echo "ERROR: install script must be run elevated"
+  echo FATAL ERROR: install script must be run elevated 
+  endlocal
   goto :eof
 )
 
-set INSTALL_PATH=%~dp0
-set HADOOP_INSTALL_DIR=%HADOOP_INSTALL_ROOT%\@final.name@
-set HADOOP_INSTALL_BIN=%HADOOP_INSTALL_DIR%\bin
+@rem java needs to be installed 
+if not exist %JAVA_HOME%\bin\java.exe (
+  echo FATAL ERROR: %JAVA_HOME%\bin\java.exe does not exist JAVA_HOME not set properly  
+  endlocal
+  goto :eof
+)
+
+@rem
+@rem  Begin install
+@rem
+echo Installing Apache Hadoop @final.name@ to %HADOOP_INSTALL_ROOT%
+echo Installing Apache Hadoop @final.name@ to %HADOOP_INSTALL_ROOT% >> %WINPKG_LOG%
 
 @rem
 @rem  Choose what services to install
+@rem    CORE_MASTER -> namenode secondarynamenode jobtracker tasktracker
+@rem    CORE_SLAVE -> datanode tasktracker
+@rem    CORE_ONEBOX || nothing -> namenode jobtracker tasktracker datanode
 @rem
 set HDFS_ROLE_SERVICES=namenode datanode secondarynamenode 
 set MAPRED_ROLE_SERVICES=jobtracker tasktracker 
 
-if "%1" == "master" (
+if "%1" == "CORE_MASTER" (
   set HDFS_ROLE_SERVICES=namenode datanode secondarynamenode 
   set MAPRED_ROLE_SERVICES=jobtracker tasktracker
 )
 
-if "%1" == "slave" ( 
+if "%1" == "CORE_SLAVE" ( 
   set HDFS_ROLE_SERVICES=datanode 
   set MAPRED_ROLE_SERVICES=tasktracker 
 )
@@ -49,32 +74,35 @@ if "%1" == "slave" (
 @rem
 @rem  Lay down the bits 
 @rem
-echo Extracting @package.zip@ to %HADOOP_INSTALL_ROOT%
-"%INSTALL_PATH%\..\resources\unzip.exe" "%INSTALL_PATH%\..\resources\@package.zip@" "%HADOOP_INSTALL_ROOT%"
-xcopy /EIYQ "%INSTALL_PATH%\..\template" "%HADOOP_INSTALL_DIR%"
+echo Extracting @package.zip@ to %HADOOP_INSTALL_ROOT% >> %WINPKG_LOG%
+"%CORE_INSTALL_PATH%\..\resources\unzip.exe" "%CORE_INSTALL_PATH%\..\resources\@package.zip@" "%HADOOP_INSTALL_ROOT%" >> "%WINPKG_LOG%"
+xcopy /EIYF "%CORE_INSTALL_PATH%\..\template" "%HADOOP_INSTALL_DIR%" >> "%WINPKG_LOG%"
 
 @rem
 @rem  Create services 
 @rem
-for %%i in (namenode datanode secondarynamenode jobtracker tasktracker ) do (
-echo Created %%i service
-copy "%INSTALL_PATH%\..\resources\winsw.exe" "%HADOOP_INSTALL_DIR%\bin\%%i.exe" /y /d 
 @rem TODO: make service run with obj= "NT Authority\Network Service"
-"%windir%\system32\sc.exe" create %%i binPath= "%HADOOP_INSTALL_BIN%\%%i.exe" DisplayName= "Hadoop %%i Service" 
+
+for %%i in (%HDFS_ROLE_SERVICES% %MAPRED_ROLE_SERVICES%) do (
+  echo Creating %%i service >> %WINPKG_LOG%
+  copy "%CORE_INSTALL_PATH%\..\resources\servicehost.exe" "%HADOOP_INSTALL_DIR%\bin\%%i.exe" /y /d  >> "%WINPKG_LOG%"
+  
+  "%windir%\system32\sc.exe" create %%i binPath= "%HADOOP_INSTALL_BIN%\%%i.exe" DisplayName= "Hadoop %%i Service"  >> "%WINPKG_LOG%"
 )
 
 @rem
 @rem  Setup HDFS service config
 @rem
+
 for %%j in (%HDFS_ROLE_SERVICES%) do (
-cmd /c "%HADOOP_INSTALL_BIN%\hdfs.cmd" --service %%j > "%HADOOP_INSTALL_BIN%\%%j.xml"
+  cmd /c "%HADOOP_INSTALL_BIN%\hdfs.cmd" --service %%j > "%HADOOP_INSTALL_BIN%\%%j.xml" 
 )
 
 @rem
 @rem  Setup MapRed service config
 @rem
 for %%k in (%MAPRED_ROLE_SERVICES%) do (
-cmd /c "%HADOOP_INSTALL_BIN%\mapred.cmd" --service %%k > "%HADOOP_INSTALL_BIN%\%%k.xml"
+  cmd /c "%HADOOP_INSTALL_BIN%\mapred.cmd" --service %%k > "%HADOOP_INSTALL_BIN%\%%k.xml"
 )
 @rem TODO create event log for services
 
--- src/packages/win/scripts/uninstall.cmd
+++ src/packages/win/scripts/uninstall.cmd
@@ -15,36 +15,43 @@
 @rem limitations under the License.
 setlocal enabledelayedexpansion
 
-set HADOOP_INSTALL_ROOT=c:\hadoop
+@rem
+@rem  Setup envrionment variables
+@rem
+if not defined HADOOP_INSTALL_ROOT ( 
+  set HADOOP_INSTALL_ROOT=c:\hadoop
+)
+if not defined CORE_UNINSTALL_PATH ( 
+  set CORE_INSTALL_PATH=%~dp0
+)
+if not defined WINPKG_LOG ( 
+  set WINPKG_LOG=%CORE_UNINSTALL_PATH%\@final.name@-winpkg.log
+)
+set HADOOP_INSTALL_DIR=%HADOOP_INSTALL_ROOT%\@final.name@
+set HADOOP_INSTALL_BIN=%HADOOP_INSTALL_DIR%\bin
 
 echo Uninstalling Apache Hadoop @version@
 
 @rem ensure running as admin.
 reg query "HKEY_USERS\S-1-5-19\Environment" /v TEMP 2>&1 | findstr /I /C:"REG_EXPAND_SZ" 2>&1 > NUL
 If %ERRORLEVEL% EQU 1 (
-  echo "ERROR: uninstall script must be run elevated"
+  echo FATAL ERROR: uninstall script must be run elevated
   goto :eof
 )
-
-set HADOOP_INSTALL_DIR=%HADOOP_INSTALL_ROOT%\@final.name@
-
-@rem
-@rem  Lay down the bits 
-@rem
 
 @rem
 @rem  Stop and delete services 
 @rem
 for %%i in (namenode datanode secondarynamenode jobtracker tasktracker ) do (
-net stop %%i
-"%windir%\system32\sc.exe" delete %%i  
+  net stop %%i >> "%WINPKG_LOG%" 
+  "%windir%\system32\sc.exe" delete %%i  >> "%WINPKG_LOG%" 
 )
 
 @rem
 @rem  Delete install dir for @final.name@
 @rem
-echo deleting %HADOOP_INSTALL_DIR%
-rd /s /q %HADOOP_INSTALL_DIR%
+echo deleting %HADOOP_INSTALL_DIR% >> "%WINPKG_LOG%"
+rd /s /q "%HADOOP_INSTALL_DIR%" >> "%WINPKG_LOG%"
 
 @rem TODO create event log for services
 
