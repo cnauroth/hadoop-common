bc9002f33cb06f776a19b201e47ee84eaecb9ccf
Steve Maine <smaine@microsoft.com>
2012-08-12 16:19:25 -0700

    Adding setup logging and fixing tons of bugs

---
+++ src/packages/win/scripts/install.ps1
@@ -0,0 +1,234 @@
+### Licensed to the Apache Software Foundation (ASF) under one or more
+### contributor license agreements.  See the NOTICE file distributed with
+### this work for additional information regarding copyright ownership.
+### The ASF licenses this file to You under the Apache License, Version 2.0
+### (the "License"); you may not use this file except in compliance with
+### the License.  You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
+param(
+	$username,
+	$password,
+	$hdfsRole,
+	$mapredRole
+	)
+
+
+function Write-Log ($message )
+{
+	Out-File -FilePath $ENV:WINPKG_LOG -InputObject $message -Append -Encoding "UTF8"
+	Write-Host $message
+}
+
+function Execute-Cmd ($command)
+{
+	Write-Log $command
+	cmd.exe /C "$command"
+}
+
+try
+{
+	if( -not (Test-Path ENV:HADOOP_NODE_INSTALL_ROOT))
+	{
+		$ENV:HADOOP_NODE_INSTALL_ROOT = "c:\hadoop"
+	}
+
+	$HDP_INSTALL_PATH = Split-Path $MyInvocation.MyCommand.Path
+	$HDP_RESOURCES_DIR = "$HDP_INSTALL_PATH\..\resources"
+	
+
+	if( -not (Test-Path ENV:WINPKG_LOG ))
+	{
+		$ENV:WINPKG_LOG="$ENV:HADOOP_NODE_INSTALL_ROOT\hadoop-@version@.winpkg.log"
+		Write-Log "Logging to $ENV:WINPKG_LOG"
+	}
+
+	$hadoopInstallDir = "$ENV:HADOOP_NODE_INSTALL_ROOT\hadoop-@version@"
+	$hadoopInstallBin = "$hadoopInstallDir\bin"
+
+	Write-Log "Username: $username"
+	Write-Log "Password: $password"
+	Write-Log "HdfsRole: $hdfsRole"
+	Write-Log "MapRedRole: $mapRedRole"
+	Write-Log "Ensuring elevated user"
+
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent( ) )
+	if ( -not ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ) ) )
+	{
+		Write-Log "$ENV:WINPKG_LOG FATAL ERROR: install script must be run elevated"
+		exit 1
+	} 
+
+	if( -not (Test-Path $ENV:JAVA_HOME\bin\java.exe))
+	{
+		Write-Log "FATAL ERROR: JAVA_HOME not set properly; $ENV:JAVA_HOME\bin\java.exe does not exist"
+		exit 1
+	}
+
+	###
+	### Root directory used for HDFS dn and nn files, and for mapdred local dir
+	###
+
+	if( -not (Test-Path ENV:HDFS_DATA_DIR))
+	{
+		$cmd = "setx HDFS_DATA_DIR $ENV:HADOOP_NODE_INSTALL_ROOT\HDFS"
+		Execute-Cmd $cmd
+	}
+
+	###
+	### Begin install
+	###
+	Write-Log "Installing Apache Hadoop hadoop-1.1.0-SNAPSHOT to $hadoopInstallDir"
+
+	if( $username -eq $null )
+	{
+		Write-Log "Invalid command line: -UserName argument is required"
+		exit 1
+	}
+
+	if( $password -eq $null )
+	{
+		Write-Log "Invalid command line: -Password is required"
+		exit 1
+	}
+
+	if( -not (Test-Path ENV:MASTER_HDFS))
+	{
+		$ENV:MASTER_HDFS = "namenode datanode secondarynamenode"
+	}
+
+	if( -not (Test-Path ENV:MASTER_MR))
+	{
+		$ENV:MASTER_MR = "jobtracker tasktracker"
+	}
+
+	if( -not (Test-Path ENV:SLAVE_MR))
+	{
+		$ENV:SLAVE_MR = "tasktracker"
+	}
+
+	if( -not (Test-Path ENV:ONEBOX_HDFS))
+	{
+		$ENV:ONEBOX_HDFS = "namenode datanode"
+	}
+
+	if( -not (Test-Path ENV:ONEBOX_MR))
+	{
+		$ENV:ONEBOX_MR = "jobtracker tasktracker"
+	}
+
+	if( $hdfsRole -eq $null )
+	{
+		$hdfsRole = "ONEBOX_HDFS"
+	}
+
+	if( $mapdredRole -eq $null )
+	{
+		$mapredRole = "ONEBOX_MR"
+	}
+
+	###
+	###  Unzip Hadoop distribution from compressed archive
+	###
+
+	Write-Log "Extracting Hadoop Core archive into $hadoopInstallDir"
+	$unzip_cmd = "$HDP_RESOURCES_DIR\unzip.exe `"$HDP_RESOURCES_DIR\hadoop-1.1.0-SNAPSHOT.zip`" `"$hadoopInstallDir`""
+	Execute-Cmd	$unzip_cmd	
+
+	$xcopy_cmd = "xcopy /EIYF `"$HDP_INSTALL_PATH\..\template`" `"$hadoopInstallDir`""
+	Execute-Cmd $xcopy_cmd
+
+	###
+	### Grant Hadoop user access to HADOOP_INSTALL_DIR and HDFS Root
+	###
+	$cmd = "icacls `"$hadoopInstallDir`" /grant ${username}:(OI)(CI)F"
+	Execute-Cmd $cmd
+
+	if( -not (Test-Path $ENV:HDFS_DATA_DIR))
+	{
+		Write-Log "Creating HDFS Data Directory $ENV:HDFS_DATA_DIR"
+		mkdir $ENV:HDFS_DATA_DIR
+	}
+
+	$cmd = "icacls `"$ENV:HDFS_DATA_DIR`" /grant ${username}:(OI)(CI)F"
+	Execute-Cmd $cmd
+
+	###
+	### Create Hadoop Windows Services and grant user ACLS to start/stop
+	###
+
+	$logonString = "obj=.\$username password=$password"
+
+	$hdfsRoleServices = $executionContext.InvokeCommand.ExpandString( "`$ENV:$hdfsRole" )
+	$mapRedRoleServices = $executionContext.InvokeCommand.ExpandString( "`$ENV:$mapredRole" )
+
+	Write-Log "Node HDFS Role Services: $hdfsRoleServices"
+	Write-Log "Node MAPRED Role Services: $mapRedRoleServices"
+
+	$allServices = $hdfsRoleServices + " " + $mapRedRoleServices
+
+	Write-Log "Installing services $allServices"
+
+	foreach( $service in $allServices.Split(' '))
+	{
+		try
+		{
+			Write-Log "Creating service $service"
+			Copy-Item "$HDP_RESOURCES_DIR\serviceHost.exe" "$hadoopInstallDir\bin\$service.exe" -Force
+
+			$cmd="$ENV:WINDIR\system32\sc.exe create $service binPath= `"$hadoopInstallDir\bin\$service.exe`" DisplayName= `"Hadoop $service`" $logonString"
+			Execute-Cmd $cmd
+
+			$cmd="$ENV:WINDIR\system32\sc.exe failure $service reset= 30 actions= restart/5000"
+			Execute-Cmd $cmd
+
+			$cmd="$ENV:WINDIR\system32\sc.exe config $service start= auto"
+			Execute-Cmd $cmd
+			}
+		finally
+		{
+		}
+	}
+
+	###
+	### Setup HDFS service config
+	###
+
+	Write-Log "Copying configuration for $hdfsRoleServices"
+
+	foreach( $service in $hdfsRoleServices.Split( ' ' ))
+	{
+		Write-Log "Creating service config ${hadoopInstallDir}\${service}.xml"
+		$cmd = "$hadoopInstallBin\hdfs.cmd --service $service > `"$hadoopInstallBin\$service.xml`""
+		Execute-Cmd $cmd		
+	}
+
+	###
+	### Setup MapRed service config
+	### 
+
+	foreach( $service in $mapRedRoleServices.Split( ' ' ))
+	{
+		Write-Log "Creating service config $hadoopInstallDir\$service.xml"
+		$cmd = "$hadoopInstallBin\mapred.cmd --service $service > `"$hadoopInstallBin\$service.xml`""
+		Execute-Cmd $cmd		
+	}
+
+	###
+	### Format the namenode
+	###
+
+	$cmd="$hadoopInstallBin\hadoop.cmd namenode -format"
+	Execute-Cmd $cmd
+}
+finally
+{
+
+}
---
+++ src/packages/win/scripts/uninstall.ps1
@@ -0,0 +1,93 @@
+### Licensed to the Apache Software Foundation (ASF) under one or more
+### contributor license agreements.  See the NOTICE file distributed with
+### this work for additional information regarding copyright ownership.
+### The ASF licenses this file to You under the Apache License, Version 2.0
+### (the "License"); you may not use this file except in compliance with
+### the License.  You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
+function Write-Log ($message )
+{
+	Out-File -FilePath $ENV:WINPKG_LOG -InputObject $message -Append -Encoding "UTF8"
+	Write-Host $message
+}
+
+function Execute-Cmd ($command)
+{
+	Write-Log $command
+	cmd.exe /C "$command"
+}
+
+try
+{
+	if( -not (Test-Path ENV:HADOOP_NODE_INSTALL_ROOT))
+	{
+		$ENV:HADOOP_NODE_INSTALL_ROOT = "c:\hadoop"
+	}
+
+	$HDP_INSTALL_PATH = Split-Path $MyInvocation.MyCommand.Path
+	$HDP_RESOURCES_DIR = "$HDP_INSTALL_PATH\..\resources"
+	
+
+	if( -not (Test-Path ENV:WINPKG_LOG ))
+	{
+		$ENV:WINPKG_LOG="$ENV:HADOOP_NODE_INSTALL_ROOT\@hadoop.core.winpkg@.log"
+		Write-Log "Logging to $ENV:WINPKG_LOG"
+	}
+
+	$hadoopInstallDir = "$ENV:HADOOP_NODE_INSTALL_ROOT\hadoop-1.1.0-SNAPSHOT"
+	$hadoopInstallBin = "$hadoopInstallDir\bin"
+
+	Write-Log "Ensuring elevated user"
+
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent( ) )
+	if ( -not ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ) ) )
+	{
+		Write-Log "$ENV:WINPKG_LOG FATAL ERROR: install script must be run elevated"
+		exit 1
+	} 
+
+	###
+	### Root directory used for HDFS dn and nn files, and for mapdred local dir
+	###
+
+	if( -not (Test-Path ENV:HDFS_DATA_DIR))
+	{
+		$ENV:HDFS_DATA_DIR = "$ENV:HADOOP_NODE_INSTALL_ROOT\HDFS"
+	}
+
+	###
+	### Stop and delete services
+	###
+
+	foreach( $service in ("namenode", "datanode", "secondarynamenode", "jobtracker", "tasktracker"))
+	{
+		Write-Log "Stopping $service"
+		$s = Get-Service $service -ErrorAction SilentlyContinue 
+
+		if( $s -ne $null )
+		{
+			Stop-Service $
+			$cmd = "sc.exe delete $service"
+			Execute-Cmd $cmd
+		}
+	}
+
+	Write-Log "Removing HDFS_DATA_DIR ($HDFS_DATA_DIR)"
+	$cmd = "rd /s /q $ENV:HDFS_DATA_DIR"
+	Execute-Cmd $cmd
+
+	Write-Log "Removing Hadoop ($hadoopInstallDir)"
+	$cmd = "rd /s /q $hadoopInstallDir"
+	Execute-Cmd $cmd
+}
+finally
+{
+}
