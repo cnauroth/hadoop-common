cd4501b401b159c21f24c16863f846ff6a0abc34
Mostafa Elhemali <mostafae@microsoft.com>
2012-12-14 16:36:27 -0800

    One more unit test for OOB operations for ASV

--- src/test/org/apache/hadoop/fs/azurenative/TestOutOfBandAzureBlobOperations.java
+++ src/test/org/apache/hadoop/fs/azurenative/TestOutOfBandAzureBlobOperations.java
@@ -11,6 +11,24 @@ import junit.framework.*;
  * the Azure Storage container from outside ASV's control.
  */
 public class TestOutOfBandAzureBlobOperations extends TestCase {
+  private AzureBlobStorageTestAccount testAccount;
+  private FileSystem fs;
+  private InMemoryBlockBlobStore backingStore;
+
+  @Override
+  protected void setUp() throws Exception {
+    testAccount = AzureBlobStorageTestAccount.createMock();
+    fs = testAccount.getFileSystem();
+    backingStore = testAccount.getMockStorage().getBackingStore();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    testAccount.cleanup();
+    fs = null;
+    backingStore = null;
+  }
+
   private static String toMockUri(String path) {
     return String.format("http://%s.blob.core.windows.net/%s/%s",
         AzureBlobStorageTestAccount.MOCK_ACCOUNT_NAME,
@@ -18,36 +36,41 @@ public class TestOutOfBandAzureBlobOperations extends TestCase {
         path);
   }
 
-  public void testImplicitFolderListed() throws Exception {
-    AzureBlobStorageTestAccount testAccount =
-        AzureBlobStorageTestAccount.createMock();
-    testAccount.getMockStorage().getBackingStore().setContent(
-        toMockUri("a/b"),
+  private void createEmptyBlobOutOfBand(String path) {
+    backingStore.setContent(
+        toMockUri(path),
         new byte[] { 1, 2 },
         new HashMap<String, String>());
+  }
+
+  public void testImplicitFolderListed() throws Exception {
+    createEmptyBlobOutOfBand("ab/b");
 
     // List the blob itself.
-    FileStatus[] obtained =
-        testAccount.getFileSystem().listStatus(new Path("/a/b"));
+    FileStatus[] obtained = fs.listStatus(new Path("/ab/b"));
     assertNotNull(obtained);
     assertEquals(1, obtained.length);
     assertFalse(obtained[0].isDir());
-    assertEquals("/a/b", obtained[0].getPath().toUri().getPath());
+    assertEquals("/ab/b", obtained[0].getPath().toUri().getPath());
 
     // List the directory
-    obtained = testAccount.getFileSystem().listStatus(new Path("/a"));
+    obtained = fs.listStatus(new Path("/ab"));
     assertNotNull(obtained);
     assertEquals(1, obtained.length);
     assertFalse(obtained[0].isDir());
-    assertEquals("/a/b", obtained[0].getPath().toUri().getPath());
+    assertEquals("/ab/b", obtained[0].getPath().toUri().getPath());
 
     // Get the directory's file status
-    FileStatus dirStatus =
-        testAccount.getFileSystem().getFileStatus(new Path("/a"));
+    FileStatus dirStatus = fs.getFileStatus(new Path("/ab"));
     assertNotNull(dirStatus);
     assertTrue(dirStatus.isDir());
-    assertEquals("/a", dirStatus.getPath().toUri().getPath());
+    assertEquals("/ab", dirStatus.getPath().toUri().getPath());
+  }
 
-    testAccount.cleanup();
+  public void testImplicitFolderDeleted() throws Exception {
+    createEmptyBlobOutOfBand("ab/b");
+    assertTrue(fs.exists(new Path("/ab")));
+    assertTrue(fs.delete(new Path("/ab"), true));
+    assertFalse(fs.exists(new Path("/ab")));
   }
 }
