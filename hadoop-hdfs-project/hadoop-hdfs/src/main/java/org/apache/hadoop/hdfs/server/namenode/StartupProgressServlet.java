864cacf28a3839fe968c32e6a86650a69a526ff6
cnauroth <cnauroth@hotmail.com>
2013-02-20 11:00:59 -0800

    HDFS-4249 - feedback - JSON for the API

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/StartupProgressServlet.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/StartupProgressServlet.java
@@ -17,12 +17,17 @@
 package org.apache.hadoop.hdfs.server.namenode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.hadoop.hdfs.server.namenode.StartupProgress.Phase;
-import org.znerd.xmlenc.XMLOutputter;
+import org.mortbay.util.ajax.JSON;
 
 @SuppressWarnings("serial")
 public class StartupProgressServlet extends DfsServlet {
@@ -31,6 +36,9 @@ public class StartupProgressServlet extends DfsServlet {
   private static final String ELAPSED_TIME = "elapsedTime";
   private static final String NAME = "name";
   private static final String PERCENT_COMPLETE = "percentComplete";
+  private static final String PHASES = "phases";
+  private static final String STATUS = "status";
+  private static final String STEPS = "steps";
   private static final String TOTAL = "total";
 
   public static final String PATH_SPEC = "/startupProgress";
@@ -39,42 +47,33 @@ public class StartupProgressServlet extends DfsServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     StartupProgress prog = NameNode.getStartupProgress();
-    XMLOutputter xml = new XMLOutputter(resp.getWriter(), "UTF-8");
-    xml.declaration();
+    resp.setContentType("application/json; charset=UTF-8");
+    List<Map<String, Object>> phases = new ArrayList<Map<String, Object>>();
 
-    try {
-      resp.setContentType("application/xml; charset=UTF-8");
-      xml.startTag("StartupProgress");
+    for (Phase phase: StartupProgress.getVisiblePhases()) {
+      Map<String, Object> phaseMap = new LinkedHashMap<String, Object>();
+      phaseMap.put(NAME, phase.getName());
+      phaseMap.put(STATUS, prog.getStatus(phase));
+      phaseMap.put(PERCENT_COMPLETE, prog.getPercentComplete(phase));
+      phaseMap.put(ELAPSED_TIME, prog.getElapsedTime(phase));
+      List<Map<String, Object>> steps = new ArrayList<Map<String, Object>>();
 
-      for (Phase phase: StartupProgress.getVisiblePhases()) {
-        xml.startTag("Phase");
-        xml.attribute(NAME, phase.getName());
-        attribute(xml, COUNT, prog.getCount(phase));
-        attribute(xml, TOTAL, prog.getTotal(phase));
-        attribute(xml, PERCENT_COMPLETE, prog.getPercentComplete(phase));
-        attribute(xml, ELAPSED_TIME, prog.getElapsedTime(phase));
-
-        for (String step: prog.getSteps(phase)) {
-          xml.startTag("Step");
-          xml.attribute(NAME, step);
-          attribute(xml, COUNT, prog.getCount(phase, step));
-          attribute(xml, TOTAL, prog.getTotal(phase, step));
-          attribute(xml, PERCENT_COMPLETE, prog.getPercentComplete(phase, step));
-          attribute(xml, ELAPSED_TIME, prog.getElapsedTime(phase, step));
-          xml.endTag();
-        }
-
-        xml.endTag();
+      for (String step: prog.getSteps(phase)) {
+        Map<String, Object> stepMap = new LinkedHashMap<String, Object>();
+        stepMap.put(NAME, step);
+        stepMap.put(COUNT, prog.getCount(phase, step));
+        stepMap.put(TOTAL, prog.getTotal(phase, step));
+        stepMap.put(PERCENT_COMPLETE, prog.getPercentComplete(phase, step));
+        stepMap.put(ELAPSED_TIME, prog.getElapsedTime(phase, step));
+        steps.add(stepMap);
       }
 
-      xml.endTag();
-    } catch (IOException e) {
-      writeXml(e, "/startupProgress", xml);
+      phaseMap.put(STEPS, steps);
+      phases.add(phaseMap);
     }
-  }
 
-  private static void attribute(XMLOutputter xml, String name, Object value)
-      throws IOException {
-    xml.attribute(name, String.valueOf(value));
+    Map<String, Object> respMap =
+      Collections.<String, Object>singletonMap(PHASES, phases);
+    resp.getWriter().println(JSON.toString(respMap));
   }
 }
