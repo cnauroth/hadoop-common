ca90d25b2e0fb6497879af48aa6fe8b3d59b6f8b
Mostafa Elhemali <mostafae@microsoft.com>
2013-03-26 13:19:43 -0700

    Fix for HADOOP-437: Remove workaround code from HADOOP-411 and use the newly introduced requestResult property on the eventData in the ResponseReceived event from the Azure Storage SDK to get my requestResult in a thread-safe manner

--- src/core/org/apache/hadoop/fs/azurenative/ResponseReceivedMetricUpdater.java
+++ src/core/org/apache/hadoop/fs/azurenative/ResponseReceivedMetricUpdater.java
@@ -4,7 +4,6 @@ import java.net.*;
 
 import com.microsoft.windowsazure.services.core.storage.*;
 import com.microsoft.windowsazure.services.core.storage.Constants.HeaderConstants;
-import com.microsoft.windowsazure.services.core.storage.utils.implementation.BaseResponse;
 
 /**
  * An event listener to the ResponseReceived event from Azure Storage that will
@@ -13,14 +12,12 @@ import com.microsoft.windowsazure.services.core.storage.utils.implementation.Bas
 class ResponseReceivedMetricUpdater extends
     StorageEvent<ResponseReceivedEvent> {
   private final AzureFileSystemInstrumentation instrumentation;
-  private final OperationContext operationContext;
   private final BandwidthGaugeUpdater blockUploadGaugeUpdater;
 
   private ResponseReceivedMetricUpdater(OperationContext operationContext,
       AzureFileSystemInstrumentation instrumentation,
       BandwidthGaugeUpdater blockUploadGaugeUpdater) {
     this.instrumentation = instrumentation;
-    this.operationContext = operationContext;
     this.blockUploadGaugeUpdater = blockUploadGaugeUpdater;
   }
 
@@ -68,28 +65,6 @@ class ResponseReceivedMetricUpdater extends
     return connection.getContentLength();
   }
 
-  private RequestResult searchForResult(String requestId) {
-    if (requestId == null) {
-      return null;
-    }
-    // This code below is not very safe: the requestResults member in
-    // operationContext is a non-thread-safe ArrayList, and the SDK can
-    // and will add results to it in other threads. I shouldn't be accessing
-    // it at all without synchronization, but I don't know how else to get
-    // my result. Best I can do for now is to not use an iterator and to get
-    // the size upfront. I think this can still break if the ArrayList resizes
-    // though - I'll take it up with the SDK people later.
-    int numberOfResults = operationContext.getRequestResults().size();
-    for (int i = numberOfResults - 1; i >= 0; i--) {
-      RequestResult currentRequest = operationContext.getRequestResults().get(i);
-      String currentRequestId = currentRequest.getServiceRequestID();
-      if (currentRequestId != null && currentRequestId.equals(requestId)) {
-        return currentRequest;
-      }
-    }
-    return null;
-  }
-
   /**
    * Handle the response-received event from Azure SDK.
    */
@@ -102,8 +77,7 @@ class ResponseReceivedMetricUpdater extends
     }
     HttpURLConnection connection =
         (HttpURLConnection)eventArg.getConnectionObject();
-    RequestResult currentResult = searchForResult(
-        BaseResponse.getRequestId(connection));
+    RequestResult currentResult = eventArg.getRequestResult();
     if (currentResult == null) {
       // Again, typically shouldn't happen, but let it pass
       return;
