40d46df85898d0d108d8c4eb42af9bae4b526ade
cnauroth <cnauroth@hotmail.com>
2014-01-08 12:42:34 -0800

    HDFS-4685-WIP: revert protobuf change in preparation for applying real fix from HDFS-5739.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/PBHelper.java
@@ -1939,16 +1939,9 @@ public class PBHelper {
       List<AclEntry> aclSpec) {
     ArrayList<AclEntryProto> r = Lists.newArrayListWithCapacity(aclSpec.size());
     for (AclEntry e : aclSpec) {
-      AclEntryProto.Builder builder = AclEntryProto.newBuilder();
-      builder.setType(convert(e.getType()));
-      if (e.getName() != null) {
-        builder.setName(e.getName());
-      }
-      if (e.getPermission() != null) {
-        builder.setPermissions(convert(e.getPermission()));
-      }
-      builder.setScope(convert(e.getScope()));
-      r.add(builder.build());
+      r.add(AclEntryProto.newBuilder().setType(convert(e.getType()))
+          .setName(e.getName()).setPermissions(convert(e.getPermission()))
+          .setScope(convert(e.getScope())).build());
     }
     return r;
   }
