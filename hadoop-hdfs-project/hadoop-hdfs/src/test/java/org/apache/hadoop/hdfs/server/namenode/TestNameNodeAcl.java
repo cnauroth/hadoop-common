b393f8e9b98a88159b6491c8a759efe956516cd0
cnauroth <cnauroth@hotmail.com>
2014-01-14 11:59:50 -0800

    HDFS-5758: stub additional tests for HDFS-5758.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestNameNodeAcl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestNameNodeAcl.java
@@ -21,6 +21,7 @@ import static org.apache.hadoop.hdfs.server.namenode.AclTestHelpers.*;
 import static org.apache.hadoop.fs.permission.AclEntryScope.*;
 import static org.apache.hadoop.fs.permission.AclEntryType.*;
 import static org.apache.hadoop.fs.permission.FsAction.*;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 
@@ -30,43 +31,203 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.permission.AclEntry;
 import org.apache.hadoop.fs.permission.AclStatus;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.AfterClass;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
+// ACL bit on
+// check does/does not have an AclFeature
+
 public class TestNameNodeAcl {
 
   private static MiniDFSCluster cluster;
   private static Configuration conf;
+  private static FileSystem fs;
+
+  private Path path;
+  private int pathCount = 0;
 
   @BeforeClass
-  public static void setUp() throws Exception {
+  public static void init() throws Exception {
     conf = new Configuration();
 
     cluster = new MiniDFSCluster.Builder(conf).numDataNodes(1).build();
     cluster.waitActive();
+    fs = cluster.getFileSystem();
   }
 
   @AfterClass
-  public static void tearDown() throws Exception {
-    cluster.shutdown();
+  public static void shutdown() throws Exception {
+    IOUtils.cleanup(null, fs);
+    if (cluster != null) {
+      cluster.shutdown();
+    }
+  }
+
+  @Before
+  public void setUp() {
+    path = new Path("/p" + ++pathCount);
+  }
+
+  @Test
+  public void testModifyAclEntries() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesOnlyAccess() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesOnlyDefault() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesMinimal() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesCustomMask() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesStickyBit() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesNullPath() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesPathNotFound() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesNullAclSpec() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testModifyAclEntriesDefaultOnFile() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntries() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesOnlyAccess() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesOnlyDefault() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesMinimal() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesStickyBit() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesNullPath() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesPathNotFound() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclEntriesNullAclSpec() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAcl() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAclOnlyAccess() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAclOnlyDefault() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAclStickyBit() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAclNullPath() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveDefaultAclPathNotFound() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAcl() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclMinimalAcl() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclStickyBit() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclNullPath() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testRemoveAclPathNotFound() throws IOException {
+    fail();
   }
 
   @Test
   public void testSetAcl() throws IOException {
-    Path p = new Path("/p");
-    FileSystem fs = cluster.getFileSystem();
-    fs.create(p).close();
+    fs.create(path).close();
     AclEntry e = new AclEntry.Builder().setName("foo")
         .setPermission(READ_EXECUTE).setScope(DEFAULT).setType(USER).build();
-    fs.setAcl(p, Lists.newArrayList(e));
-    AclStatus s = fs.getAclStatus(p);
+    fs.setAcl(path, Lists.newArrayList(e));
+    AclStatus s = fs.getAclStatus(path);
     AclEntry[] returned = Lists.newArrayList(s.getEntries()).toArray(
         new AclEntry[0]);
-    Assert.assertArrayEquals(new AclEntry[] {
+    assertArrayEquals(new AclEntry[] {
         aclEntry(ACCESS, USER, READ_WRITE),
         aclEntry(ACCESS, GROUP, READ),
         aclEntry(ACCESS, OTHER, READ),
@@ -75,5 +236,50 @@ public class TestNameNodeAcl {
         aclEntry(DEFAULT, GROUP, READ),
         aclEntry(DEFAULT, MASK, READ_EXECUTE),
         aclEntry(DEFAULT, OTHER, READ) }, returned);
+  }
+
+  @Test
+  public void testSetAclOnlyAccess() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclOnlyDefault() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclMinimal() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclCustomMask() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclStickyBit() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclNullPath() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclPathNotFound() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclNullAclSpec() throws IOException {
+    fail();
+  }
+
+  @Test
+  public void testSetAclDefaultOnFile() throws IOException {
+    fail();
   }
 }
